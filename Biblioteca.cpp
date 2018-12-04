144428496965ad7daede4b15fe35f59e64191e74
william2305-d <william2305@hotmail.com>
2018-12-04 13:06:54 -0200

    inicio da implementação das classes de  biblioteca

---
+++ Biblioteca.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "Biblioteca.h"
+using namespace std;
+
---
+++ Biblioteca.h
@@ -0,0 +1,57 @@
+#include <iostream>
+namespace std;
+
+class Biblioteca{
+	public:
+	private:
+};
+
+class emprestimo: public Biblioteca{
+	private:
+		int numero;
+		
+	public:
+
+
+};
+
+class usuario{
+	private:
+	public:
+		string nome;
+		string cpf;
+		string endereco;
+		string fone;
+		usuario();
+};
+
+class publicacao{
+	private:
+	public:
+		int codPublicacao;
+		string titulo;
+		string editora;
+		int ano;
+};
+
+class itemEmprestimo{
+	public:
+				
+	private:
+		itemEmprestimo();
+};
+
+class livro{
+	public:
+	private:
+		
+};
+
+class periodico{
+
+};
+
+int main(){
+
+	return 0;
+}
