a0fbb924e65c699837cbdba14f5a50f61d9981da
william2305-d <william2305@hotmail.com>
2018-12-05 21:46:38 -0200

    organizando as classes

--- Biblioteca.h
+++ Biblioteca.h
@@ -1,42 +1,82 @@
 #include <string.h>
 #include <iostream>
+#include <time.h>
 namespace std;
 
-
-class emprestimo{
-		
+class Date{
 	public:
-		int numero;
-		Date dataEmprestimo;
-		Usuario usuario;//como criar esse objeto?
-		int proximoNumero;//como torná-lo estático?
-		emprestimo(int numero, Date dataEmprestimo, Date dataPrevDevolucao, Usuario usuario);
-		void adicionarLivro(livro nomeLivro);
-		void excluirLivro(livro nomeLivro);
-		void devolverLivro(livro nomeLivro);
-		void devolverTodosOsLivros();
-
+		int dia;
+		int mes;
+		int ano;
+		Date();
+		~Date(){};
 
 };
 
 class Usuario{
 	private:
-	public:
 		string nome;
 		string cpf;
 		string endereco;
 		string fone;
-		Usuario();
-		~Usuario();
+		Date dataPenalizacao;
+	public:
+		Usuario(string nome, string cpf, string endereco,string fone);
+		~Usuario(){};
 };
 
 class Publicacao{
 	private:
-	public:
 		int codPublicacao;
 		string titulo;
 		string editora;
 		int ano;
+	pubic:
+		Publicacao();
+		Publicacao(int codP, string tit, string ed, int a);
+		~Publicacao(){}; 
+};
+
+class Livro:public Publicacao{
+	private:
+		string autores;
+		int qtdeExemplares;
+	public:
+		Livro(string aut, int codP, string tit, string ed, int a,int qtdeE = 0):autores(aut),codPublicacao(codP),titulo(tit),editora(ed),ano(a),qtdeExemplares(qtdeE){};
+		~Livro(){};
+		void incrementar();
+		void decrementar();
+		
+};
+
+class Periodico:public Publicacao{
+	private:
+		int numEdicao;
+		string mes;
+	public:
+		Periodico(int numE, string m, int codP, string tit, string ed, int a):numeEdicao(numE), mes(m), codPublicacao(codP),titulo(tit),editora(ed),ano(a){};
+		~Periodico(){};
+};
+
+
+class emprestimo{
+		
+	private:
+		int numero;
+		Date dataEmprestimo;
+		Usuario usuario;
+		int proximoNumero;
+		Date dataPrevDevolucao;
+		vector <ItemEmprestimo> itens;
+	public:
+		void adicionarLivro(Livro nomeLivro);
+		void excluirLivro(Livro nomeLivro);
+		void devolverLivro(Livro nomeLivro);
+		void devolverTodosOsLivros();
+		emprestimo(int num, Date dataE, Usuario usu, Date dataPrevDev);
+
+
+
 };
 
 class Biblioteca{
@@ -55,26 +95,6 @@ class itemEmprestimo{
 		Date dataDevolucao;
 		Livro livro;
 		itemEmprestimo(Livro Livro, Date dataDevolucao = NULL);
-};
-
-class Livro{
-	private:
-	public:
-		string autores;
-		int qtdeExemplares;
-		livro(string autoresPar, int qtdeExemplaresPar);
-		livro(int codPublicacao, string titulo, string editora, int ano, string autores, int qtdeExemplares = 0);
-		void acrescentar();
-		void decrementar();
-		
-};
-
-class periodico{
-	private:
-	public:
-		int numEdicao;
-		string mes;
-		periodico(int numEdicaoPar, string mesPar);
 };
 
 int main(){
