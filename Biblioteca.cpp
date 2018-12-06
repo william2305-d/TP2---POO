98857ddd5ca844271370f4a7ddd3a5df4e1f14a9
manu4566 <manu4566@gmail.com>
2018-12-06 16:41:04 -0200

    Parte da Biblioteca em andamento

--- Biblioteca.cpp
+++ Biblioteca.cpp
@@ -12,6 +12,15 @@ int Date::operator==(Date &d){
 	return false;
 };
 
+//USUARIO
+
+int Usuario::operator==(const Usuario &u)const{
+	if( nome == u.nome && cpf == u.cpf && endereco == u.endereco && fone == u.fone){
+		return true;
+	}
+	return false;
+}
+
 //LIVRO
 
 //Esta errado so fiz pr testar, falta testar se as outras variaveis sao iguais
@@ -34,14 +43,23 @@ Livro ItemEmprestimo::getLivro(){
 	return livro;
 }
 
-//Contador 
-int Emprestimo::proximoNumero = 0;
+
 
 //EMPRESTIMO 
 
+//Contador 
+int Emprestimo::proximoNumero = 0;
+
 Emprestimo::Emprestimo(Usuario usu, Date dataPrevDev):usuario(usu),dataPrevDevolucao(dataPrevDev){
+	Date a;
+	a.DataCorrente();
+// Tirar De Comentario apos implementar getDataPen();
+//	if(usu.getDataPen() <= a ){
+//		throw Erro("Usuario esta Penalizado");
+//	}
 	numero = proximoNumero;
-	//Inicializar DataEmprestimo com a Data Corrente (Will)
+	dataEmprestimo.DataCorrente();
+	
 }
 
 void Emprestimo::adicionarLivro(Livro &nomeLivro){
@@ -112,4 +130,82 @@ void Emprestimo::devolverTodosOsLivros(){
 	}
 	
 }
+
+//BIBLIOTECA
+
+//Construtor para inicializar as listas
+Biblioteca::Biblioteca(){
+	//Mesmo ao inicializar, ja começa como vazio, decidi remendicionar para 0;
+	emprestimos.resize(0);
+	usuarios.resize(0);
+	livros.resize(0);
+}
+
+//Metodos para inserir
+
+void Biblioteca::novoUsuario(const Usuario &u){
+	usuarios.push_back(u);
+}
+
+void Biblioteca::novoEmprestimo(const Emprestimo &e){
+	emprestimos.push_back(e);
+}
+
+void Biblioteca::novaPublicacao(const Publicacao &p){ //Precisa de função virtual ? Para livro ser reconcedo como publicação?
+	livros.push_back(p);
+}
+
+void Biblioteca::novoItemEmprestimo(Emprestimo &e, ItemEmprestimo &i){
+	e.adcItemEmprestimo(i);
+}
+
+//Metodos para exlusão 
+//Todos tem seu tratamento de excessões!!! Lembrar de usar try and catch no main
+
+void Biblioteca::excluiUsuario(const Usuario &u){
+ //Primeiro ,tem que saber se o usuario existe
+ //Fazer uma busca no vetor usuarios  
+ 	int j;
+	for(j= 0; j < usuarios.size(); j++){
+		if(usuarios[j] == u){
+			break; //usuario existe
+		}
+	}
+	
+	//Verifica se fez uma busca em todo e não encontrou o usuario
+	if(j == usuarios.size()){
+		throw Erro("Usuario não existe");
+	}
+
+//Se acha=lo, verificar se ele tem algum emprestimo
+//Para isso, é preciso acessar o usuario nas lista de emprestimos OK
+//Tem que ter uma sobrecarga no operador na classe de usuario == OK
+	int i;
+	for( i = 0; i < emprestimos.size() ; i++){ // no vetor vai até size-1 pq começa em 0
+		if(emprestimos[i].getUsuario() == u){ //Achou o usuario na lista de emprestimo
+			Date hoje;
+			hoje.DataCorrente();
+		//Tirar depois que implementar operador <=
+		//	if(emprestimos[i].getDataPrevDev() <= hoje){ // Verifica se ele tem emprestimo 
+		//		throw Erro("Usuario não pode ser excluido, ele possui emprestimo");
+		//	}
+			usuarios.erase(usuarios.begin() + j); //Deleto o Usuario
+			break;	
+		}
+	}
+	
+	//Usuario não foi achado na lista de emprestimos
+	if(i == emprestimos.size()){
+		usuarios.erase(usuarios.begin() + j);
+	}
+
+
+}
+
+
+
+
+
+
+
 
